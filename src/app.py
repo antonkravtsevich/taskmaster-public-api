8150a39d0b58864cdb02b80a8701fc9f3c98c3a9
Anton <antonkravtsevich@gmail.com>
2018-04-02 16:09:23 +0300

    refactored, add field validation and tests

--- src/app.py
+++ src/app.py
@@ -1,35 +1,86 @@
-from flask import Flask, jsonify, request
+import flask
+from flask import jsonify, request
 from flask_cors import CORS
-import requests
-from pymongo import MongoClient
 import os
+from pymongo import MongoClient
+import requests
 
-app = Flask(__name__)
-CORS(app)
-SA_SERVICE_HOST = os.environ.get('SA_SERVICE_HOST', 'localhost')
+ENV = os.environ.get('ENV', 'production')
+if ENV == 'test':
+    SA_SERVICE_PORT = '31206'
+    SA_SERVICE_HOST = '188.166.115.138'
+else:
+    SA_SERVICE_PORT = '5000'
+    SA_SERVICE_HOST = os.environ.get('SA_SERVICE_HOST', 'localhost')
+
 MONGODB_HOST = os.environ.get('MONGODB_HOST', 'localhost')
 MONGODB_DATABASE = os.environ.get('MONGODB_DATABASE', 'tmdata')
 
-#create connection
-try:
-    client = MongoClient(MONGODB_HOST, 27017)
-except:
-    print("Can't create connection")
-    os._exit(1)
+app = flask.Flask(__name__)
+CORS(app)
 
-db = client[MONGODB_DATABASE]
-posts = db['posts']
+
+def resp(code, data):
+    return (jsonify(data), code)
+
+
+def check_request(request, fields):
+    errors = []
+    if not request.json:
+        errors.append("No JSON sent. Did you forget to set Content-Type header" +
+            " to application/json?")
+        return errors
+
+    for field in fields:
+        if not field['name'] in request.json:
+            errors.append('Field "{}" is missing'.format(field['name']))
+        else:
+            if type(request.json[field['name']]) is not field['type']:
+                errors.append('Field "{}" must be {}'.format(field['name'], field['type']))
+    return errors
+
+
+def get_collection():
+    try:
+        client = MongoClient(MONGODB_HOST, 27017)
+    except:
+        print("Can't create connection")
+        os._exit(1)
+
+    db = client[MONGODB_DATABASE]
+    posts = db['posts']
+    return posts
 
 
 def get_polarity(text):
     dict_to_send={'raw_text':text}
-    res = requests.post('http://'+SA_SERVICE_HOST+':5000', json=dict_to_send)
+    res = requests.post('http://'+SA_SERVICE_HOST+':'+SA_SERVICE_PORT, json=dict_to_send)
     print(res)
     json = res.json()
     return(json['polarity'])
 
 
-# REMOVE THAT AFTER TEST!!!
+def add_to_database(collection, theme, text, polarity):
+    post={'theme':theme, 'text':text, 'polarity':polarity}
+    try:
+        collection.insert_one(post)
+    except:
+        print('error in add to mongo')
+        os._exit(1)
+
+
+def get_posts_from_database(collection, theme=None):
+    if theme:
+        result_cursor = collection.find({'theme':theme})
+    else:
+        result_cursor = collection.find()
+    result = []
+    for doc in result_cursor:
+        del doc['_id']
+        result.append(doc)
+    return result
+
+
 @app.route('/get_env_var', methods=['GET'])
 def get_env_var():
     text = 'SA_SERVICE_HOST: {}\n'.format(SA_SERVICE_HOST)
@@ -38,33 +89,29 @@ def get_env_var():
 
 @app.route('/posts', methods=['POST'])
 def add_new_post():
-    json = request.get_json(silent=True)
-    theme = json['theme']
-    text = json['text']
+    required_fileds = [
+        {'name': 'text', 'type': str},
+        {'name': 'theme', 'type': str}
+    ]
+    errors = check_request(request=request, fields=required_fileds)
+    if errors:
+        return resp(code=400, data={'status':'error', 'errors':errors})
+    
+    theme = request.json['theme']
+    text = request.json['text']
     polarity = get_polarity(text)
-    post={'theme':theme, 'text':text, 'polarity':polarity}
-
-    print('Theme: {}\nText: {}\nPolarity: {}'.format(theme, text, polarity))
-    try:
-        post_id = posts.insert_one(post).inserted_id
-    except:
-        print('error in add to mongo')
-        os._exit(1)
+    add_to_database(collection=app.config['collection'], theme=theme, text=text, polarity=polarity)
     return(jsonify({'status': 'ok'}), 200)
 
 
 @app.route('/posts/<theme>', methods=['GET'])
 def get_posts_by_theme(theme):
-    result_cursor = posts.find({'theme':theme})
-    result = []
-    for doc in result_cursor:
-        del doc['_id']
-        result.append(doc)
-    print(result)
+    result = get_posts_from_database(collection=app.config['collection'], theme=theme)
     return(jsonify({'status':'ok', 'result':result}), 200)
 
 
 def main():
+    app.config['collection'] = get_collection()
     app.run(host='0.0.0.0', port=8080)
 
 
---
+++ test.py
@@ -0,0 +1,24 @@
+import unittest
+import src.app as app
+
+class TestAPI(unittest.TestCase):
+
+    def __init__(self, *args, **kwargs):
+        super(TestAPI, self).__init__(*args, **kwargs)
+        self.collection = app.get_collection()
+
+
+    def test_add_to_database(self):
+        theme = 'theme'
+        text = 'text'
+        polarity = 0.0
+        app.add_to_database(collection=self.collection, theme=theme, text=text, polarity=polarity)
+        result = app.get_posts_from_database(collection=self.collection, theme=theme)
+        self.assertEqual(len(result), 1)
+        self.assertEqual(theme, result[0]['theme'])
+        self.assertEqual(text, result[0]['text'])
+        self.assertEqual(polarity, result[0]['polarity'])
+
+
+if __name__ == '__main__':
+    unittest.main()
