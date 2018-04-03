201d2c68320d9d37f11b17fc9b2345206d5376e1
Anton <antonkravtsevich@gmail.com>
2018-04-03 13:10:08 +0300

    fix test

--- test.py
+++ test.py
@@ -11,8 +11,9 @@ class TestAPI(unittest.TestCase):
     def test_add_to_database(self):
         theme = 'theme'
         text = 'text'
+        timestamp = '1522747384843'
         polarity = 0.0
-        app.add_to_database(collection=self.collection, theme=theme, text=text, polarity=polarity)
+        app.add_to_database(collection=self.collection, timestamp=timestamp, theme=theme, text=text, polarity=polarity)
         result = app.get_posts_from_database(collection=self.collection, theme=theme)
         self.assertEqual(len(result), 1)
         self.assertEqual(theme, result[0]['theme'])
