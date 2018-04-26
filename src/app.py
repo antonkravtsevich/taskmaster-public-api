cd2668c9408abfa4a8e71c989568e4864b75f67e
Anton <antonkravtsevich@gmail.com>
2018-04-26 06:17:28 +0300

    add cors

--- src/app.py
+++ src/app.py
@@ -11,8 +11,10 @@ from tm import ThematicModeller
 from sa import SentimentalAnalyzer
 import os
 from datetime import datetime
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.debug = True
 app.config['SECRET_KEY'] = 'ephch5a6qd5XeRubD'
 
